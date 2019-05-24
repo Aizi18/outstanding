739b3e1d24769e301b6f3d470ea7e8865ebb5542
Unknown <sarra.aizi@infinitedevelopers.eu>
2019-05-24 11:13:51 +0200

    fixture et liste des articles

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,8 +9,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+     for($i = 1; $i <= 10 ; $i++){
+        $article = new Article();
+        $article->setTitle("Titre de l'article n°$i")
+                ->setContent("<p>Contenu de l'article n° $i</p>")
+                ->setImage("http://placehols.it/350x150")
+                ->setCreatedAt(new \DateTime());
+                $manager->persist($article);
+     }
 
         $manager->flush();
     }
