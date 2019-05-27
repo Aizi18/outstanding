4be0ed65a871ca0d74b4323b94a6435285d80103
Unknown <sarra.aizi@infinitedevelopers.eu>
2019-05-27 15:02:28 +0200

    formulaire d'ajout d'articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use App\Repository\ArticleRepository;
 class BlogController extends AbstractController
 {
@@ -32,6 +34,29 @@ class BlogController extends AbstractController
         ]);
     }
     /**
+     * @Route("/blog/new", name="blog_create")
+     */
+    public function create(Request $request, ObjectManager $manager) //ajouter au use en haut de la page 
+    {
+        $article =new Article();
+        $form = $this->createFormBuilder($article)
+                     ->add('title', TextType::class , [
+                         'attr' => [
+                             'placeholder' => "Titre de l'article"
+                         ]
+                    ]) //pour créer nos propre champs :->add('title',TextType::class) avec un use Symfony\Component\Form\Extension\Core\Type\TextType;
+                     ->add('content') //la fonction add permet d'ajouter des champs
+                     ->add('image') 
+                     ->getForm();   
+        return $this->render('blog/create.html.twig',[ //on passe à twig le formulaire tous simplement resultat createForm
+                'formArticle' => $form->createView() // dans create.html.twig je passe cette variable 
+        ]);
+    }
+
+
+
+
+    /**
      * @Route("/blog/{id}", name="blog_show")
      */
     public function show(Article $article)
@@ -40,4 +65,5 @@ class BlogController extends AbstractController
             'article' => $article
         ]);
     }
+     
 }
